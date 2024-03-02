431548120c25356cd77f38201b796c2240e53d4f
Carcinophobic <bardinasjacob@gmail.com>
2024-03-02 06:27:23 -0500

    Adding Mongo Connection Boilerplate

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,11 +1,20 @@
 import connectDB from "../connect";
-import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) {
-  try {
-    await connectDB();
-
-  } catch (error: any) {
-    return "Error";
-  }
+interface SampleData{
+    message: string;
 }
+
+export async function GET(req: Request, res: Response) {
+    try{
+        await connectDB
+        const data: SampleData = {
+            message: "Sample Message",
+        };
+        res = Response.json(data)
+        return res;
+    }
+    catch(error){
+        console.error("Error handling request ", error);
+        res.json()
+    }
+  }
--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,33 @@ import Button from '@mui/joy/Button';
 import React, {useState} from "react";
 import CharacterButtons from "./components/CharacterButtons";
 import Navigation from "./components/Navigation";
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.SERVER_URI
+const client = new MongoClient(uri);
 
 
 
 export default function Home() {
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
+
   return (
     <main className="w-full">
       <div className="m-auto w-1/2 space-y-28 mt-9">
         <Navigation/>
         <CharacterButtons/>
       </div>
+  
     </main>
   );
 }
