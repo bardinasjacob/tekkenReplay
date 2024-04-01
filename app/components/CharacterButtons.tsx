c663647fbeb5840dbc524e97ad2233c6ece269a4
Carcinophobic <bardinasjacob@gmail.com>
2024-04-01 00:22:39 -0400

    Adding Character Portraits

--- app/components/CharacterButtons.tsx
+++ app/components/CharacterButtons.tsx
@@ -13,14 +13,17 @@ import {
 } from "@mui/material";
 
 function CharacterButtons() {
-  var selected: string[] = ["Mokujin", "Mokujin"];
   const [drops, setDrops] = useState<{ [key: string]: string }>({});
 
+  var selected: string[] = [];
+
+  //Putting all different dropdown menus into key value pairs to send to character array
   const handleChange = (event: SelectChangeEvent) => {
     const { name, value } = event.target;
     setDrops((prevDrops) => ({ ...prevDrops, [name]: value }));
   };
 
+  //Creating the dropdown menus for the characters
   function createButton(buttonNum: number) {
     return (
       <FormControl fullWidth className=" bg-stone-600">
@@ -36,25 +39,53 @@ function CharacterButtons() {
           onChange={handleChange}
         >
           <MenuItem value={"undefined"}>Any</MenuItem>
-          <MenuItem value={"Dragunov"}>Dragunov</MenuItem>
           <MenuItem value={"Kazuya"}>Kazuya</MenuItem>
-          <MenuItem value={"BeepBoop"}>Thirty</MenuItem>
+          <MenuItem value={"Jin"}>Jin</MenuItem>
+          <MenuItem value={"King"}>King</MenuItem>
+          <MenuItem value={"Jun"}>Jun</MenuItem>
+          <MenuItem value={"Paul"}>Paul</MenuItem>
+          <MenuItem value={"Law"}>Law</MenuItem>
+          <MenuItem value={"Jack-8"}>Jack-8</MenuItem>
+          <MenuItem value={"Lars"}>Lars</MenuItem>
+          <MenuItem value={"Xiaoyu"}>Xiaoyu</MenuItem>
+          <MenuItem value={"Nina"}>Nina</MenuItem>
+          <MenuItem value={"Leroy"}>Leroy</MenuItem>
+          <MenuItem value={"Asuka"}>Asuka</MenuItem>
+          <MenuItem value={"Lili"}>Lili</MenuItem>
+          <MenuItem value={"Bryan"}>Bryan</MenuItem>
+          <MenuItem value={"Hwoarang"}>Hwoarang</MenuItem>
+          <MenuItem value={"Claudio"}>Claudio</MenuItem>
+          <MenuItem value={"Azucena"}>Azucena</MenuItem>
+          <MenuItem value={"Raven"}>Raven</MenuItem>
+          <MenuItem value={"Leo"}>Leo</MenuItem>
+          <MenuItem value={"Steve"}>Steve</MenuItem>
+          <MenuItem value={"Kuma"}>Kuma</MenuItem>
+          <MenuItem value={"Yoshimitsu"}>Yoshimitsu</MenuItem>
+          <MenuItem value={"Shaheen"}>Shaheen</MenuItem>
+          <MenuItem value={"Dragunov"}>Dragunov</MenuItem>
+          <MenuItem value={"Feng"}>Feng</MenuItem>
+          <MenuItem value={"Panda"}>Panda</MenuItem>
+          <MenuItem value={"Lee"}>Lee</MenuItem>
+          <MenuItem value={"Alisa"}>Alisa</MenuItem>
+          <MenuItem value={"Zafina"}>Zafina</MenuItem>
+          <MenuItem value={"Devil Jin"}>Devil Jin</MenuItem>
+          <MenuItem value={"Victor"}>Victor</MenuItem>
+          <MenuItem value={"Reina"}>Reina</MenuItem>
         </Select>
       </FormControl>
     );
   }
 
-  selected[0] = drops.drop1
-  selected[1] = drops.drop2
+  selected[0] = drops.drop1;
+  selected[1] = drops.drop2;
 
   return (
     <>
       <div className="flex w-full flex-wrap justify-between">
         <div className="w-1/3">
           <Stack>
-            <div className="w-full relative h-52">
-            <Image src={`/${selected[0]}.png`} alt={""}
-             fill={true}/>
+            <div className="w-full relative h-80">
+              <Image src={`/${selected[0]}.png`} alt={""} fill={true} />
             </div>
             {createButton(1)}
           </Stack>
@@ -63,10 +94,9 @@ function CharacterButtons() {
           <h1>VS</h1>
         </div>
         <div className="w-1/3">
-        <Stack>
-            <div className="w-full relative h-52">
-            <Image src={`/${selected[1]}.png`} alt={""}
-             fill={true}/>
+          <Stack>
+            <div className="w-full relative h-80">
+              <Image src={`/${selected[1]}.png`} alt={""} fill={true} />
             </div>
             {createButton(2)}
           </Stack>
